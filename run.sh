7c7d3181f56407574473743a709954cb667ed5ba
Yoan Arnaudov <jonidev@gmail.com>
2013-11-28 14:11:54 +0200

    added arguments -c to the run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,38 @@
-#!/bin/sh
-while getopts "h?vf:" opt; do
+#!/bin/bash
+showUsage () {
+    echo 'Usage: run.sh [arguments]'
+    echo ''
+}
+showArguments () {
+    echo 'Arguments:'
+    echo '       -c                    Number of children screenshot taking processes'
+}
+
+while getopts "h?c:" opt; do
     case "$opt" in
         h|\?)
-            echo 'HELP'
-            show_help
+            showUsage
+            showArguments
             exit 0
             ;;
-        v)  verbose=1
-            echo 'VERBOSE'
-            ;;
-        f)  output_file=$OPTARG
-            echo $OPTARG
-            echo 'F'
+        c)  childrenProcesses=$OPTARG
             ;;
     esac
 done
-exit
+
+if [ -z "$*" ]; then
+    childrenProcesses=1
+fi
 
 pidsList=""
 
 ./webserver.py > /dev/null 2> /dev/null &
 pidsList="$pidsList $!"
-phantomjs screenshot.js > /dev/null 2> /dev/null &
-pidsList="$pidsList $!"
+for (( i=1; i <= childrenProcesses; i++ ))
+do
+    phantomjs screenshot.js > /dev/null 2> /dev/null &
+    pidsList="$pidsList $!"
+done
 
 trap "kill $pidsList" exit INT TERM
 wait
