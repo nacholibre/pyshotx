6b293d251790160342763140f7a8319b89ff9ef7
Yoan Arnaudov <jonidev@gmail.com>
2013-12-06 15:54:17 +0200

    added exception catch debug

--- webserver.py
+++ webserver.py
@@ -37,86 +37,91 @@ def resizeDaemon(redisConnection, screenshotsQueueKey, resizeQueueKey,
                  sw, parentPid, useLevels):
     secretWord = sw
     while True:
-        #check if parent process exists, if not kill children
-        if not os.path.exists("/proc/%s" % parentPid):
-            sys.exit()
+        try:
+            #check if parent process exists, if not kill children
+            if not os.path.exists("/proc/%s" % parentPid):
+                sys.exit()
 
-        resizeRedis = redisConnection.spop(resizeQueueKey)
-        if resizeRedis is None:
-            time.sleep(1)
-        else:
-            domain, screenshotsForResizing = resizeRedis.split('@')
-            thumbnailsList = dict()
+            resizeRedis = redisConnection.spop(resizeQueueKey)
+            if resizeRedis is None:
+                time.sleep(1)
+            else:
+                domain, screenshotsForResizing = resizeRedis.split('@')
+                thumbnailsList = dict()
 
-            screenshotsJson = json.loads(screenshotsForResizing)
+                screenshotsJson = json.loads(screenshotsForResizing)
 
-            for deviceName, screenshot in screenshotsJson.iteritems():
-                screenshotPath = screenshot
-                thumbnailsList[deviceName] = dict()
-                thumbnailsList[deviceName]['screenshot'] = None
-                thumbnailsList[deviceName]['thumbnail'] = None
+                for deviceName, screenshot in screenshotsJson.iteritems():
+                    screenshotPath = screenshot
+                    thumbnailsList[deviceName] = dict()
+                    thumbnailsList[deviceName]['screenshot'] = None
+                    thumbnailsList[deviceName]['thumbnail'] = None
 
-                #check if phantomjs has created screenshot for this device
-                if not screenshot:
-                    continue
+                    #check if phantomjs has created screenshot for this device
+                    if not screenshot:
+                        continue
 
-                #check if screenshot really exists
-                try:
-                    open(screenshotPath, 'r')
-                except Exception:
-                    continue
+                    #check if screenshot really exists
+                    try:
+                        open(screenshotPath, 'r')
+                    except Exception:
+                        continue
 
-                split = screenshotPath.split('/')
-                filename = split[-1]
-                del split[-1]
-                savePath = '/'.join(split)
-                domainDevice = filename.replace('.png', '')
-                domain, device = domainDevice.split('_')
+                    split = screenshotPath.split('/')
+                    filename = split[-1]
+                    del split[-1]
+                    savePath = '/'.join(split)
+                    domainDevice = filename.replace('.png', '')
+                    domain, device = domainDevice.split('_')
 
-                domainHash = hashlib.md5(domain + secretWord).hexdigest()
+                    domainHash = hashlib.md5(domain + secretWord).hexdigest()
 
-                levels = '/%s/%s/%s/%s' % (domainHash[0],
-                                           domainHash[1],
-                                           domainHash[2],
-                                           domainHash[3])
+                    levels = '/%s/%s/%s/%s' % (domainHash[0],
+                                               domainHash[1],
+                                               domainHash[2],
+                                               domainHash[3])
 
-                if useLevels:
-                    savePath = savePath + '%s' % levels
-                thumbnailFilename = '%s/%s_%s_thumbnail.jpg' % (savePath,
-                                                                domainHash,
-                                                                device)
-                screenshot = ProcessScreenshot.open(screenshotPath)
-                if device == 'iPhone':
-                    screenshot.saveThumbnail(1180,
-                                             210,
-                                             thumbnailFilename,
-                                             quality=60)
-                elif device == 'iPad':
-                    screenshot.saveThumbnail(342,
-                                             256,
-                                             thumbnailFilename,
-                                             quality=60)
-                elif device == 'laptop':
-                    screenshot.saveThumbnail(232,
-                                             139,
-                                             thumbnailFilename,
-                                             quality=60)
+                    if useLevels:
+                        savePath = savePath + '%s' % levels
+                    thumbnailFilename = '%s/%s_%s_thumbnail.jpg' % (savePath,
+                                                                    domainHash,
+                                                                    device)
+                    screenshot = ProcessScreenshot.open(screenshotPath)
+                    if device == 'iPhone':
+                        screenshot.saveThumbnail(1180,
+                                                 210,
+                                                 thumbnailFilename,
+                                                 quality=60)
+                    elif device == 'iPad':
+                        screenshot.saveThumbnail(342,
+                                                 256,
+                                                 thumbnailFilename,
+                                                 quality=60)
+                    elif device == 'laptop':
+                        screenshot.saveThumbnail(232,
+                                                 139,
+                                                 thumbnailFilename,
+                                                 quality=60)
 
-                screenshotFilename = '%s/%s_%s.jpg' % (savePath,
-                                                       domainHash,
-                                                       device)
-                screenshot = ProcessScreenshot.open(screenshotPath)
-                screenshot.save(screenshotFilename, 60)
+                    screenshotFilename = '%s/%s_%s.jpg' % (savePath,
+                                                           domainHash,
+                                                           device)
+                    screenshot = ProcessScreenshot.open(screenshotPath)
+                    screenshot.save(screenshotFilename, 60)
 
-                thumbnailsList[deviceName] = dict()
-                thumbnailsList[deviceName]['screenshot'] = screenshotFilename
-                thumbnailsList[deviceName]['thumbnail'] = thumbnailFilename
+                    thumbnailsList[deviceName] = dict()
+                    thumbnailsList[deviceName]['screenshot'] = screenshotFilename
+                    thumbnailsList[deviceName]['thumbnail'] = thumbnailFilename
 
-                os.remove(screenshotPath)
+                    os.remove(screenshotPath)
 
-            redisKey = '%s_done' % domain
-            redisConnection.set(redisKey, json.dumps(thumbnailsList))
-            redisConnection.expire(redisKey, 60)
+                redisKey = '%s_done' % domain
+                redisConnection.set(redisKey, json.dumps(thumbnailsList))
+                redisConnection.expire(redisKey, 60)
+        except Exception, err:
+            print 'EXCEPTION, ERROR'
+            print err
+            sys.stdout.flush()
 
 
 def waitForScreenshot(domain, screenshotsQueue):
